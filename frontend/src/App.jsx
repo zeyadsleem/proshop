77ca82721686f459cad626df3b56db3734be7ae6
zeyadsleem <zeyadsleem1@gmail.com>
2024-03-06 09:26:20 +0200

    finish product page, and it works

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Container } from "react-bootstrap";
-import Header from "./components/Header";
+import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
+import Header from "./components/Header";
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
       <Footer />
