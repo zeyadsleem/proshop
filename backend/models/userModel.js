978c13a24c422aa8c23a66eb3f8c6d5618f36478
zeyadsleem <zeyadsleem1@gmail.com>
2024-03-11 04:09:34 +0200

    add user api contolers

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,14 +1,32 @@
 import { Schema, model } from "mongoose";
+import bcrypt from "bcryptjs";
 
-const userSchema = new Schema(
+const userSchema = Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    isAdmin: { type: Boolean, required: true, defalt: false },
+    isAdmin: { type: Boolean, required: true, default: false },
   },
-  { timestamps: true },
+  {
+    timestamps: true,
+  },
 );
+
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Encrypt password using bcrypt
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = bcrypt.hash(this.password, salt);
+});
 
 const User = model("User", userSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,38 @@
 import dotenv from "dotenv";
 import express from "express";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
 
-const PORT = process.env.PORT;
+const port = process.env.PORT | 8000;
 
 connectDB();
 
 const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
+
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/", (_, res) => {
   res.send("Api testing server is running ...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`),
+);
