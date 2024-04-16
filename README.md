a5a1dff698d2163b64aa497dfb852083dc9de828
zeyadsleem <zeyadsleem1@gmail.com>
2024-04-16 06:19:35 +0200

    fix: change frontend build from 'build/' to vite 'dist/'

---
+++ README.md
@@ -0,0 +1,3 @@
+# proshop mern ecommerce app
+
+visit live demo ➡ [proshop](https://proshop-y5kj.onrender.com/)
--- backend/server.js
+++ backend/server.js
@@ -32,10 +32,10 @@ app.get("/api/config/paypal", (req, res) =>
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
   app.use("/uploads", express.static("/var/data/uploads"));
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")),
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html")),
   );
 } else {
   const __dirname = path.resolve();
